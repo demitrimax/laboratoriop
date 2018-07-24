7a5a35ba6ce40a5e810f3902cf5d9ec70c292b67
demitrimax <armandoaguilar1@yahoo.com>
2018-07-24 10:43:36 -0500

    Tabla de Clientes, primera migración

---
+++ database/migrations/2018_07_23_164522_create_clientes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('RFC',15);
+            $table->string('domicilio');
+            $table->string('ciudad',10);
+            $table->string('telefono1',10);
+            $table->string('telefono2',10);
+            $table->string('correo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
