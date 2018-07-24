d45f88548c04b301ae64444e08674955eb3d8cf8
demitrimax <armandoaguilar1@yahoo.com>
2018-07-24 14:03:22 -0500

    se agrega catalogo de motivos

---
+++ database/migrations/2018_07_24_185359_create_catmotivos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCatmotivosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catmotivos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->strings('motivo')->coment('Motivo de la prueba');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catmotivos');
+    }
+}
