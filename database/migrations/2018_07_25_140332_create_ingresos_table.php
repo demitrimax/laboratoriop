a377e860e84968cb63a4cdf895dc5960c7331496
demitrimax <armandoaguilar1@yahoo.com>
2018-07-25 09:56:11 -0500

    tabla de ingreso modificada

--- database/migrations/2018_07_25_140332_create_ingresos_table.php
+++ database/migrations/2018_07_25_140332_create_ingresos_table.php
@@ -16,15 +16,26 @@ class CreateIngresosTable extends Migration
         Schema::create('ingresos', function (Blueprint $table) {
             $table->increments('id');
             $table->date('date');
-            $table->integer('idmvz');
-
-            $table->integer('cliente');
+            $table->integer('idmvz')->unsigned();
+                //foreing
+                $table->foreign('idmvz')->references('id')->on('medicos_m_v_zs');
+            $table->integer('cliente')->unsigned();
             //foreing
-            $table->integer('especie');
+                $table->foreign('cliente')->references('id')->on('clientes');
+            $table->integer('especie')->unsigned();
             //foreint
+                $table->foreign('especie')->references('id')->on('cat_especies');
             $table->string('raza')->nullable();
-            $table->string('sexo',100);
-            $table->integer('num_muestras');
+            $table->string('sexo',100)->nullable();
+            $table->integer('num_muestras')->nullable();
+            $table->integer('mot_prueba')->unsigned();
+            //foreing
+                $table->foreign('mot_prueba')->references('id')->on('catmotivos');
+            $table->integer('analisis')->unsigned();
+            //foreing
+              $table->foreign('analisis')->references('id')->on('catservicios');
+            $table->string('tratamiento');
+            $table->string('observaciones');
             $table->timestamps();
         });
     }
